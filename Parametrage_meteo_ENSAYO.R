a2626f6293aea32f811926f1efa73af030b8dd3b
VEZY R <vezy.remi@gmail.com>
2015-06-12 15:46:24 -0600

    Variable names and Variables types fixed
    MAESPA doesn't handle factors variables, neither character variable.
    Removed Date_fac and hour fac. Replaced it by semi-hours (integer from 1
    to 48).

--- Parametrage_meteo_ENSAYO.R
+++ Parametrage_meteo_ENSAYO.R
@@ -9,10 +9,8 @@ library(Maeswrap)
 source("F:/These/Projects/R-Functions/Maeswrap_cor_VR.R")
 PathMeteo= "F:/These/Projects_classified/ENSAYO_Transmittance_Map/DATA/METEO"
 Meteoraw=read.table(paste(PathMeteo,"/OutDF.GlobalMetFullSunEnsayo.csv", sep=''), h=T, sep=";")
-head(Meteoraw)
 
 Meteorawdata= Meteoraw[Meteoraw$DOY>65&Meteoraw$DOY<158,]
-head(Meteorawdata)
 SWC= rowMeans(cbind(Meteorawdata$VW.1._m3H2Om3, Meteorawdata$VW.2._m3H2Om3, 
                     Meteorawdata$VW.3._m3H2Om3, Meteorawdata$VW.4._m3H2Om3), na.rm=T)
 SWC[is.nan(SWC)]=NA
@@ -20,18 +18,17 @@ SWC[is.nan(SWC)]=NA
 
 #Interpolate missing water content:
 library(zoo)
-SWC= na.approx(SWC)
+SWC= round(na.approx(SWC),3)
 Meteorawdata$DiffuseFrSpitters[is.na(Meteorawdata$DiffuseFrSpitters)]=1
 Meteorawdata$RH_pc[Meteorawdata$RH_pc>100]=100
+Meteorawdata$VPD3m
 
-
-MAESPA_var= data.frame(DATE= Meteorawdata$Date_fac, DOY=Meteorawdata$DOY, 
-                       HOUR= Meteorawdata$Hour_fac, WIND= Meteorawdata$WindSpeed3m, 
-                       TAIR= Meteorawdata$AirTC_degC, RHperc= Meteorawdata$RH_pc, 
-                       PAR= Meteorawdata$PAR_micE, FBEAM= Meteorawdata$DiffuseFrSpitters,
-                       PRESS= Meteorawdata$P, SWC, PPT=Meteorawdata$Rain_mm_Tot)
+MAESPA_var= data.frame(DOY=Meteorawdata$DOY, HOUR= rep(1:48, length(Meteorawdata$DOY)),
+                       WIND= round(Meteorawdata$WindSpeed3m,3), TAIR= round(Meteorawdata$AirTC_degC, 3),
+                       RHperc= round(Meteorawdata$RH_pc,2), PAR= round(Meteorawdata$PAR_micE,2), 
+                       FBEAM= round(Meteorawdata$DiffuseFrSpitters,3),
+                       PRESS= round(Meteorawdata$P,2), SWC, PPT=Meteorawdata$Rain_mm_Tot)
 summary(is.na(MAESPA_var))
-
 
 
 
@@ -43,32 +40,15 @@ summary(is.na(MAESPA_var))
 # Replace met.dat -----------------------------------------------------------------------------
 
 metdat= "F:/These/Projects_classified/ENSAYO_Transmittance_Map/DATA/Fichiers_dat/Originaux_Aquiares/met.dat"
-test=readmet(newmetdat)
-head(test)
 newmetdat= "F:/These/Projects_classified/ENSAYO_Transmittance_Map/DATA/Fichiers_dat/Nouveaux_ENSAYO/met.dat"
 replacemetdata_VR(metdfr=MAESPA_var, oldmetfile = metdat, newmetfile = newmetdat)
-
-readLines(metdat)
-
 replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "nocolumns", ncol(MAESPA_var))
-replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "startdate", MAESPA_var$DATE[1])
-replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "enddate", tail(MAESPA_var$DATE,1))
+replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "startdate", Meteorawdata$Date_fac[1])
+replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "enddate", tail(Meteorawdata$Date_fac,1))
 
 # Replace the RHperc by RH% for the columns names in the met.dat
 RHloc= grep(paste("RH"), trim((colnames(MAESPA_var))))  #find the position of the character RH
 columns= colnames(MAESPA_var)                           #Set the names of the columns
 columns[RHloc]= "RH%"                                   #Replace the RHperc by RH%
 replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "columns", newval = columns)
-
-
-
-
-
-
-
-
-
-
-
-
 
