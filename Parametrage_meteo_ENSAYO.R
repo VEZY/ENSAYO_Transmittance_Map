34e8861f722d5cc540472b9ae7622bdad860e5fe
VEZY R <vezy.remi@gmail.com>
2015-06-15 19:33:30 -0600

    Jour 15 Juin 2015
    Avancements

--- Parametrage_meteo_ENSAYO.R
+++ Parametrage_meteo_ENSAYO.R
@@ -23,12 +23,13 @@ Meteorawdata$DiffuseFrSpitters[is.na(Meteorawdata$DiffuseFrSpitters)]=1
 Meteorawdata$RH_pc[Meteorawdata$RH_pc>100]=100
 Meteorawdata$VPD3m
 
-MAESPA_var= data.frame(DOY=Meteorawdata$DOY, HOUR= rep(1:48, length(Meteorawdata$DOY)),
+MAESPA_var= data.frame(DOY=Meteorawdata$DOY, HOUR= rep(1:48, (length(Meteorawdata$DOY)/48)),
                        WIND= round(Meteorawdata$WindSpeed3m,3), TAIR= round(Meteorawdata$AirTC_degC, 3),
                        RHperc= round(Meteorawdata$RH_pc,2), PAR= round(Meteorawdata$PAR_micE,2), 
                        FBEAM= round(Meteorawdata$DiffuseFrSpitters,3),
                        PRESS= round(Meteorawdata$P,2), SWC, PPT=Meteorawdata$Rain_mm_Tot)
 summary(is.na(MAESPA_var))
+
 
 
 
@@ -41,7 +42,7 @@ summary(is.na(MAESPA_var))
 
 metdat= "F:/These/Projects_classified/ENSAYO_Transmittance_Map/DATA/Fichiers_dat/Originaux_Aquiares/met.dat"
 newmetdat= "F:/These/Projects_classified/ENSAYO_Transmittance_Map/DATA/Fichiers_dat/Nouveaux_ENSAYO/met.dat"
-replacemetdata_VR(metdfr=MAESPA_var, oldmetfile = metdat, newmetfile = newmetdat)
+replacemetdata_VR(metdfr=MAESPA_var, oldmetfile = newmetdat, newmetfile = newmetdat)
 replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "nocolumns", ncol(MAESPA_var))
 replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "startdate", Meteorawdata$Date_fac[1])
 replacePAR_VR(datfile = newmetdat, namelist = "metformat", parname = "enddate", tail(Meteorawdata$Date_fac,1))
--- Parametrage_points_PAR_transmittance.R
+++ Parametrage_points_PAR_transmittance.R
@@ -29,23 +29,40 @@ Table_points= as.data.frame(cbind(X_position=round((Coord_All_Coffees$X-minX),3)
 # replacePAR(datfile = points, namelist = "CONTROL", parname = "NOPOINTS", newval = nrow(Coord_All_Coffees))
 # replacePAR(datfile = points, namelist = "XYZ", parname = "COORDS", newval = Table_points)
 
+
+# Days of simulation choice -------------------------------------------------------------------
+# Two contrasted days in terms of FBEAM.
+
+meteo= readmet("F:/These/Projects_classified/ENSAYO_Transmittance_Map/DATA/Fichiers_dat/Nouveaux_ENSAYO/met.dat")
+meteo[which(meteo$FBEAM<0.25&meteo$PAR>2000),]
+#Le jour 88 est un jour intéressant puisqu'il présente un PAR très élevé  et un FBEAM assez bas.
+head(meteo[which(meteo$FBEAM>0.9&meteo$FBEAM<1&meteo$PAR>800),],10)
+#Le jour 76 aussi car il a un FBEAM proche de 1 (mais pas =1, qui sont des gap-fil),et un PAR assez élevé.
+
+
+# MAESPA Call ---------------------------------------------------------------------------------
+
 confile= paste(Path_sensible_data, "/MAESPA_routine/MAESPA_Remko_VR/confile.dat", sep='')
 points= paste(Path_sensible_data, "/MAESPA_routine/MAESPA_Remko_VR/points.dat", sep='')
 NPoints= nrow(Coord_All_Coffees)
-replacePAR_VR(datfile=confile, parname = "startdate", namelist = "dates", "07/03/2015")
-replacePAR_VR(datfile=confile, parname = "enddate", namelist = "dates", "07/03/2015")
+replacePAR_VR(datfile=confile, parname = "startdate", namelist = "dates", "17/03/2015")
+replacePAR_VR(datfile=confile, parname = "enddate", namelist = "dates", "17/03/2015")
 replacePAR_VR(datfile = points, namelist = "CONTROL", parname = "NOPOINTS", newval = NPoints)
 replacePAR_VR(datfile = points, namelist = "XYZ", parname = "COORDS", newval = Table_points) 
 system(paste(Path_sensible_data,'/MAESPA_routine/MAESPA_Remko_VR/MAESPA_Remko_VR', sep=''), show.output.on.console=T)
-Results= readtestflx(paste(Path_sensible_data,"/MAESPA_routine/MAESPA_Remko_VR/testflx.dat", sep=""))
+
+Results_29_03= readtestflx(paste(Path_sensible_data,"/MAESPA_routine/MAESPA_Remko_VR/testflx_29_03.dat", sep=""))
+Results_17_03= readtestflx(paste(Path_sensible_data,"/MAESPA_routine/MAESPA_Remko_VR/testflx_17_03.dat", sep=""))
 
 
 # Prepares the results ---------------------------------------------------------------------
 
 library(raster)
 #Rajouter les Y et X min de la parcelle en coordonn?es WGS 84 (cf Parametrage_Trees_ENSAYO_OR2 l.281)
-Results$X= Results$X + minX
-Results$Y= Results$Y + minY
+Results_29_03$X= Results_29_03$X + minX
+Results_29_03$Y= Results_29_03$Y + minY
+Results_17_03$X= Results_17_03$X + minX
+Results_17_03$Y= Results_17_03$Y + minY
 # Defines the system projection:
 crsplot="+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +units=m +no_defs" 
 #ref: http://spatialreference.org/ref/epsg/wgs-84-utm-zone-17n/proj4/
@@ -59,22 +76,27 @@ Raster_plot= raster(nrows=350, ncols=518, xmn=minX, xmx=(518+minX), ymn=minY, ym
 
 # Make a mean table by point (mean of the days and the hours) ---------------------------------
 
-Results_jourmoy= aggregate(Results[,-c(1:3,6)], list(Results[,3]), mean)
+Results_jourmoy_29_03= aggregate(Results_29_03[,-c(1:3,6)], list(Results_29_03[,3]), mean)
+Results_jourmoy_17_03= aggregate(Results_17_03[,-c(1:3,6)], list(Results_17_03[,3]), mean)
 
 # Create a raster layer and plot it -----------------------------------------------------------
 
-head(Results_jourmoy)
-temp_Results_APAR= cbind(x=Results_jourmoy$X, y=Results_jourmoy$Y, z=(Results_jourmoy$TTOT))
-APAR_plot=Raster_plot
-cell1 <- cellFromXY(APAR_plot, temp_Results_APAR[,1:2])
-# finds the corresponding coordinates of temp_Results in APAR_plot
-APAR_plot[cell1] <- temp_Results_APAR[,3]
-# writeRaster(APAR_plot, "Results/APAR_plot2.asc", format="ascii", overwrite=T)
-APAR_plot=raster("Results/APAR_plot.asc")
+temp_Results_TTOT_29= cbind(x=Results_jourmoy_29_03$X, y=Results_jourmoy_29_03$Y, z=(Results_jourmoy_29_03$TTOT))
+TTOT_plot_29=Raster_plot
+cell1 <- cellFromXY(TTOT_plot_29, temp_Results_TTOT_29[,1:2])
+# finds the corresponding coordinates of temp_Results in TTOT_plot_29
+TTOT_plot_29[cell1] <- temp_Results_TTOT_29[,3]
+# writeRaster(TTOT_plot_29, "Results/TTOT_plot_292.asc", format="ascii", overwrite=T)
+# TTOT_plot_29=raster("Results/TTOT_plot_29.asc")
+plot(TTOT_plot_29, col=terrain.colors(100), main=expression(Incident~PAR~(µmol.m^-2~.s^-1)))
+plot(TTOT_plot_29, col=rainbow(100, start=0, end=0.9), main=expression(Incident~PAR~(µmol.m^-2~.s^-1)))
 
-plot(APAR_plot, col=terrain.colors(100), main=expression(Incident~PAR~(µmol.m^-2~.s^-1)))
-plot(APAR_plot, col=rainbow(100, start=0, end=0.9), main=expression(Incident~PAR~(µmol.m^-2~.s^-1)))
 
+temp_Results_TTOT_17= cbind(x=Results_jourmoy_17_03$X, y=Results_jourmoy_17_03$Y, z=(Results_jourmoy_17_03$TTOT))
+TTOT_17_plot=Raster_plot
+cell1 <- cellFromXY(TTOT_17_plot, temp_Results_TTOT_17[,1:2])
+TTOT_17_plot[cell1] <- temp_Results_TTOT_17[,3]
+plot(TTOT_17_plot, col=terrain.colors(100), main=expression(Incident~PAR~(µmol.m^-2~.s^-1)))
 
 
 # Control the legend's scale and color --------------------------------------------------------
@@ -84,7 +106,7 @@ breakpoints <- seq(0, 450, 35)
 #colors <- rainbow(100, start=0, end=1)
 colors= rev(terrain.colors(16))
 # colors <- gray.colors(14, start=0, end=1)
-plot(APAR_plot,breaks=breakpoints,col=colors, main='TestFlux APAR (umol m-2 s-1)')
+plot(TTOT_plot,breaks=breakpoints,col=colors, main='TestFlux APAR (umol m-2 s-1)')
 
 # Add the ENSAYO Trees on the map:
 # TREES <- readOGR(dsn="F:/These/Projects_classified/GIS_ENSAYO", layer="Trees_B3_MS")
@@ -96,31 +118,53 @@ plot(APAR_plot,breaks=breakpoints,col=colors, main='TestFlux APAR (umol m-2 s-1)
 
 # Make maps by hour ---------------------------------------------------------------------------
 
-head(Results)
-NBHours= 
-
-breakpoints <- seq(0, 1200, 50)
-breakpoints= breakpoints[-c(2:5)]
-colors <- rainbow(100, start=0, end=1)
-
-Rasters_Trans= vector("list", length(levels(as.factor(Results$HR))))
-Rasters_Trans[[1]]= test
-minHour= min(Results$HR)
-maxHour= max(Results$HR)
-x11()
-for (i in minHour:maxHour){
-Results_Hour= Results[Results$HR==i,]
-Results_Hour_APAR= cbind(x=Results_Hour$X, y=Results_Hour$Y, z=(Results_Hour$TTOT))
-Hour_APAR_plot=Raster_plot
-cell1 <- cellFromXY(Hour_APAR_plot, Results_Hour_APAR[,1:2])
-Hour_APAR_plot[cell1] <- Results_Hour_APAR[,3]
-filename= paste("Tranmittance_Hour_", i, ".tiff", sep="")
-mypath=file.path("F:/These/Projects_classified/ENSAYO_Transmittance_Map/Results", filename)
-tiff(file=mypath,  width= 1311 , height= 840, res=96)
-Title = bquote(Semi-Hour~N~degree~.(i)~Incident~PAR~(µmol.m^-2~.s^-1))
-plot(Hour_APAR_plot, main=Title)
-dev.off()
+Daily_testflux= function(X, TargetVariable, Filepath, FileName, VariableTitle){
+    minHour= min(X$HR)
+    maxHour= max(X$HR)
+    Rasters_Trans= vector("list", length(levels(as.factor(Results$HR))))
+    
+    for (i in minHour:maxHour){
+        Results_Hour_X= cbind(x=X$X[X$HR==i], y=X$Y[X$HR==i], z=TargetVariable[X$HR==i])
+        Hour_X_plot=Raster_plot
+        cell1 <- cellFromXY(Hour_X_plot, Results_Hour_X[,1:2])
+        Hour_X_plot[cell1] <- Results_Hour_X[,3]
+        filename= paste(FileName, i, ".tiff", sep="")
+        tiff(file=file.path(Filepath, filename),  width= 1311 , height= 840, res=96)
+        Title = bquote(Semi-Hour~N~degree~.(i)~.(VariableTitle))
+        plot(Hour_X_plot, main=Title, colNA="azure3", col=(terrain.colors(255)))
+        dev.off()
+    }
 }
+
+TargetVariable= Transmittance_29
+Transmittance_29= Results_29_03$TTOT / Results_29_03$PAR
+Transmittance_17= Results_17_03$TTOT / Results_17_03$PAR
+mypath="F:/These/Projects_classified/ENSAYO_Transmittance_Map/Results/TIFF"
+Daily_testflux(Results_29_03, Transmittance_29, Filepath= mypath, FileName= "Tranmittance_Hour_",
+               VariableTitle= bquote(Transmittance~("[0-1]")~DOY~88~Low~FBEAM))
+Daily_testflux(X= Results_29_03, Results_29_03$TD, Filepath= mypath, FileName= "Diffuse_Tranmittance_Hour_",
+               VariableTitle= bquote(Diffuse~Transmittance~("[0-1]")~DOY~88~Low~FBEAM))
+Daily_testflux(Results_17_03, Transmittance_17, Filepath= mypath, FileName= "Tranmittance_Hour_",
+               VariableTitle= bquote(Transmittance~("[0-1]")~DOY~76~High~FBEAM))
+Daily_testflux(X= Results_17_03, Results_17_03$TD, Filepath= mypath, FileName= "Diffuse_Tranmittance_Hour_",
+               VariableTitle= bquote(Diffuse~Transmittance~("[0-1]")~DOY~76~High~FBEAM))
+levels(as.factor(X$HR))
+
+# Mesures comparison --------------------------------------------------------------------------
+# Measures where made with hemispheric images.
+
+Table_measures_temp= read.table(paste(Path_sensible_data,"/DATA/Coresp.csv", sep=""),
+                              h=T, sep=";", numerals = "no.loss")
+
+Table_meas= aggregate(Table_measures_temp, list(Table_measures_temp$ID_cafe_PER), mean)
+temp_Results_Meas_temp= cbind(x=Table_meas$X_PER, y=Table_meas$Y_PER, z=Table_meas$DiffTransmittance)
+temp_Results_Meas= temp_Results_Meas_temp[which(!is.na(temp_Results_Meas_temp[,1])),]
+Meas_plot=Raster_plot
+cell1 <- cellFromXY(Meas_plot, temp_Results_Meas[,1:2])
+Meas_plot[cell1] <- temp_Results_Meas[,3]
+plot(Meas_plot, add=T, col='red')
+
+
 
 
 
